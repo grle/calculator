6adc94a970744d452195c698ca873712a2998c01
Grace Lee <graceleenexus@gmail.com>
2022-08-25 16:20:45 -0700

    condense javascript

--- script.js
+++ script.js
@@ -1,341 +1,157 @@
-//WINDOW
-function operate(num1, num2, op) {
-  switch(op) {
-    case "add":
-      return num1 + num2;
-      break;
-    case "subt":
-      return num1 - num2;
-      break;
-    case "mult":
-      return num1 * num2;
-      break;
-    case "div":
-      if (num2 == 0) {
-        return null;
-      }
-      else {
-        return num1 / num2;
-      }
-      break;
+const buttons = document.getElementsByClassName("btn");
+let boolDot = false;
+let num1 = "";
+let num2 = "";
+let op = "";
+let concatStr = "";
+
+const getButtons = () => {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", (event) => {
+      setEquation(buttons[i].innerHTML);
+    });
+  }
+}
+getButtons();
+
+function setEquation(str) {
+  //filter out numbers
+  if (str == "CLEAR") {
+    btnClear();
+    return;
+  } else if (str == "DELETE") {
+    btnDelete();
+    return;
+  } else if (str == "+" || str == "-" || str == "x" || str == "÷") {
+    btnOperate(str);
+    return;
+  } else if (str == "=") {
+    btnEqual();
+    return;
+  } else if (str == ".") {
+    btnDot();
+    return;
+  }
+
+  if (num1.length < 13) {
+    let getNum1 = num1.concat(str);
+    num1 = getNum1;
+    document.getElementById("eqEnter").innerHTML = num1;
   }
 }
 
-
-//...9999 x 9999999999999 =
-
-function addOn(addString) {
-
-  //concatonating the right operator
-  let concatOp = "";
-
-  switch(addString) {
-    case "add":
-      concatOp = " + ";
-      break;
-    case "subt":
-      concatOp = " - ";
-      break;
-    case "mult":
-      concatOp = " x ";
-      break;
-    case "div":
-      concatOp = " / ";
-      break;
+function btnOperate(str) {
+  //one number
+  if (num2 == "") {
+    concatStr = num1.concat(` ${str} `);
+    num2 = num1;
   }
 
-  let length = numArray.length;
+  //two numbers
+  else {
+    let n1 = Number(num1);
+    let n2 = Number(num2);
 
-  if (length == 1) {
-    eqString = numString.concat(concatOp);
-    document.getElementById("eqConcat").innerHTML = eqString;
-    numString = "";
-  }
-  else if (length == 2) {
-    let hold1 = Number(numArray[0]);
-    let hold2 = Number(numArray[1]);
-    let concatHold = op[0];
-
-    let total = operate(hold1, hold2, concatHold);
-    if (total == null) {
-      document.getElementById("eqEnter").innerHTML = "UNDEFINED";
-      //add other things here to clean up the visuals
+    let result = myOp(op, n2, n1);
+    //check UNDEFINED
+    if (result == "UNDEFINED") {
+      concatStr = `${n2} ${op} ${n1} = `;
+      document.getElementById("eqEnter").innerHTML = result;
+      document.getElementById("eqConcat").innerHTML = concatStr;
+      return;
     }
 
-    op.shift();
-    let numTotal = total.toString();
-    numArray = [];
-    numArray.push(numTotal);
-    eqString = numTotal.concat(concatOp);
-    document.getElementById("eqConcat").innerHTML = eqString;
-    numString = "";
+    num2 = result.toString();
+    concatStr = num2.concat(` ${str} `);
   }
+  op = str;
+  num1 = "";
+  document.getElementById("eqConcat").innerHTML = concatStr;
+}
+
+function myOp(str, n2, n1) {
+  let n = 0;
+  switch (str) {
+    case "+":
+      n = add(n2, n1);
+      break;
+    case "-":
+      n = sub(n2, n1);
+      break;
+    case "x":
+      n = mul(n2, n1);
+      break;
+    case "÷":
+      n = div(n2, n1);
+      break;
+  }
+  return n;
 }
 
 function btnEqual() {
-  if (boolOp == true) {
-    return;
-  }
+  let n1 = Number(num1);
+  let n2 = Number(num2);
+  let result = myOp(op, n2, n1);
 
-  //eqConcat
-  let result = eqString.concat(numString);
-  document.getElementById("eqConcat").innerHTML = result.concat(" = ");
+  //limit digits concatStr "...9999 x 9999999999999 = "
+  concatStr = `${n2} ${op} ${n1} = `;
+  document.getElementById("eqEnter").innerHTML = result;
+  document.getElementById("eqConcat").innerHTML = concatStr;
 
-  //eqEnter
-  numArray.push(numString);
-  let hold1 = Number(numArray[0]);
-  let hold2 = Number(numArray[1]);
-  let concatHold = op[0];
-  let total = operate(hold1, hold2, concatHold);
-  if (total == null) {
-    document.getElementById("eqEnter").innerHTML = "UNDEFINED";
-    //figure out what to do here
-  }
-  let numTotal = total.toString();
-  numArray = [];
-  // numArray.push(numTotal);
-  document.getElementById("eqEnter").innerHTML = numTotal;
-  // eqString = numTotal;
-  numString = numTotal;
-  op.shift();
-}
-
-function btnClear() {
+  //CLEAR
+  num2 = "";
+  num1 = "";
   boolDot = false;
-  numString = "";
-  eqString = "";
-  numArray = [];
-  op = [];
-  document.getElementById("eqEnter").innerHTML = "&nbsp;";
-  document.getElementById("eqConcat").innerHTML = "&nbsp;";
-}
-
-//ARITHMETICS
-let op = [];
-let numArray = [];
-let boolDot = false;
-let boolOp = false;
-let numString = "";
-let eqString = "";
-
-//operators
-function btnAdd() {
-  if (numString == "") {
-    return;
-  }
-  deleteDot();
-  boolDot = false;
-  boolOp = true;
-  op.push("add");
-  numArray.push(numString);
-  addOn("add");
-}
-
-function btnSubt() {
-  if (numString == "") {
-    return;
-  }
-  deleteDot();
-  boolDot = false;
-  boolOp = true;
-  op.push("subt");
-  numArray.push(numString);
-  addOn("subt");
-}
-
-function btnMult() {
-  if (numString == "") {
-    return;
-  }
-  deleteDot();
-  boolDot = false;
-  boolOp = true;
-  op.push("mult");
-  numArray.push(numString);
-  addOn("mult");
-}
-
-function btnDiv() {
-  if (numString == "") {
-    return;
-  }
-  deleteDot();
-  boolDot = false;
-  boolOp = true;
-  op.push("div");
-  numArray.push(numString);
-  addOn("div");
-}
-
-//buttons
-function btnDelete() {
-  if (numString == "") {
-    boolDot = false;
-    return;
-  } else if (numString.length == 1) {
-    console.log(numString.length);
-    boolDot = false;
-    numString = "";
-    document.getElementById("eqEnter").innerHTML = "0";
-    return;
-  }
-  else if (numString == "UNDEFINED") {
-    btnClear();
-    return;
-  }
-
-  //if you delete dot, you should be able to retype it
-  if (isDot() == true) {
-    boolDot = false;
-  }
-
-  //delete last char in string
-  let del = numString.length - 1;
-  let result = numString.substring(0, del);
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = numString;
-}
-
-//DOT
-function isDot() {
-  let del = numString.length - 1;
-  let checkDot = numString.substring (del, numString.length);
-  if (checkDot == ".") {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-
-function deleteDot() {
-  let del = numString.length - 1;
-  let checkDot = numString.substring (del, numString.length);
-  if (checkDot == ".") {
-    let result = numString.substring(0, del);
-    numString = result;
-  }
-  else {
-    return;
-  }
+  concatStr = "";
 }
 
 function btnDot() {
-  // if (numString == "") {
-  //   return;
-  // }
-  if (numString.length > 12) {
-    return;
-  }
-
-  if (boolDot == false) {
+  if (boolDot == true) return;
+  if (num1.length < 13) {
+    let getNum = num1.concat(".");
+    num1 = getNum;
+    document.getElementById("eqEnter").innerHTML = num1;
     boolDot = true;
-
-    let result = numString.concat(".");
-    numString = result;
-    document.getElementById("eqEnter").innerHTML = result;
-  } else {
-    return;
   }
 }
 
-//NUMBERS
-function zero() {
-  if (numString.length > 12) {
-    return;
-  }
-  boolOp = false;
-  let result = numString.concat("0");
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = result;
+function btnClear() {
+  num1 = "";
+  num2 = "";
+  boolDot = false;
+  concatStr = "";
+  document.getElementById("eqConcat").innerHTML = "&nbsp;";
+  document.getElementById("eqEnter").innerHTML = 0;
 }
 
-function one() {
-  if (numString.length > 12) {
-    return;
+function btnDelete() {
+  let del = num1.length - 1;
+  let result = num1.substring(0, del);
+
+  //check dot
+  let checkDot = num1.substring(del, num1.length);
+  if (checkDot == ".") boolDot = false;
+
+  num1 = result;
+  document.getElementById("eqEnter").innerHTML = num1;
+  if (num1.length == 0) {
+    document.getElementById("eqEnter").innerHTML = 0;
   }
-  boolOp = false;
-  let result = numString.concat("1");
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = result;
 }
 
-function two() {
-  if (numString.length > 12) {
-    return;
-  }
-  boolOp = false;
-  let result = numString.concat("2");
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = result;
+function add(a, b) {
+  return a + b;
 }
 
-function three() {
-  if (numString.length > 12) {
-    return;
-  }
-  boolOp = false;
-  let result = numString.concat("3");
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = result;
+function sub(a, b) {
+  return a - b;
 }
 
-function four() {
-  if (numString.length > 12) {
-    return;
-  }
-  boolOp = false;
-  let result = numString.concat("4");
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = result;
+function mul(a, b) {
+  return a * b;
 }
 
-function five() {
-  if (numString.length > 12) {
-    return;
-  }
-  boolOp = false;
-  let result = numString.concat("5");
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = result;
-}
-
-function six() {
-  if (numString.length > 12) {
-    return;
-  }
-  boolOp = false;
-  let result = numString.concat("6");
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = result;
-}
-
-function seven() {
-  if (numString.length > 12) {
-    return;
-  }
-  boolOp = false;
-  let result = numString.concat("7");
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = result;
-}
-
-function eight() {
-  if (numString.length > 12) {
-    return;
-  }
-  boolOp = false;
-  let result = numString.concat("8");
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = result;
-}
-
-function nine() {
-  if (numString.length > 12) {
-    return;
-  }
-  boolOp = false;
-  let result = numString.concat("9");
-  numString = result;
-  document.getElementById("eqEnter").innerHTML = result;
+function div(a, b) {
+  if (b == 0) return "UNDEFINED";
+  return a / b;
 }
